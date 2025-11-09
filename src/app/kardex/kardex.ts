3f394f062817a4af8a91e2ef216734ce4869da65
Octaviofg <48572130+OctavioFloG@users.noreply.github.com>
2025-11-09 17:26:25 -0600

    Create kardex component

--- src/app/kardex/kardex.ts
+++ src/app/kardex/kardex.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../services/api/api.service';
 @Component({
   selector: 'app-kardex',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './kardex.html',
-  styleUrl: './kardex.css',
+  styleUrl: './kardex.css'
 })
-export class KardexComponent {
+export class KardexComponent implements OnInit {
+  loading = true;
+  errorMsg = '';
+  materias: any[] = [];
 
+  constructor(private api: ApiService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.api.get<any>('movil/kardex').subscribe({
+      next: (res) => {
+        console.log('[Kardex] Respuesta API:', res);
+        if (res.code === 200 && res.data?.length > 0) {
+          this.materias = res.data;
+        } else {
+          this.errorMsg = 'No se encontraron registros en el kardex.';
+        }
+        this.loading = false;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error('[Kardex] Error:', err);
+        this.errorMsg = 'Error al cargar kardex.';
+        this.loading = false;
+        this.cdr.detectChanges();
+      }
+    });
+  }
 }
